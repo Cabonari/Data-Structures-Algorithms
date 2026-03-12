9920777ca1287bf5cebf2406e27b0009c41c9951
Jullian0908 <j.goncalves0908@gmail.com>
2026-03-12 15:11:52 +0100

    kleine start array

--- obj/Debug/net9.0/Data-Structures-Algorithms.AssemblyInfo.cs
+++ obj/Debug/net9.0/Data-Structures-Algorithms.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data-Structures-Algorithms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90c63ae1c779b7cbb8dccd8788ce8dd891bf7f9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cffbf16b21fee1dbf5195bac322b363c218e7b9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data-Structures-Algorithms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data-Structures-Algorithms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
