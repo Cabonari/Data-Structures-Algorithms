13e99afcae0eadecabbf2f1fabc5c9ac18fd13c9
Vanderflys <85625559+Vanderflys@users.noreply.github.com>
2026-03-12 15:18:58 +0100

    s

--- obj/Debug/net9.0/Data-Structures-Algorithms.AssemblyInfo.cs
+++ obj/Debug/net9.0/Data-Structures-Algorithms.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data-Structures-Algorithms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+298513328f973b4d1ba2e3728bd204fce98f4c71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83a21425a48b21762313f201f34749f37bc98674")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data-Structures-Algorithms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data-Structures-Algorithms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
