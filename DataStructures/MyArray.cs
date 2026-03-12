b3a7a64cab622e010cfa14bf587d76fd63437aba
Jullian0908 <j.goncalves0908@gmail.com>
2026-03-12 15:51:39 +0100

    add and remove array functions

--- DataStructures/MyArray.cs
+++ DataStructures/MyArray.cs
@@ -11,11 +11,33 @@ public class MyArray<T> : IMyCollection<T>
     }
     public int Count => _size;
     //add function
+    public void Add(T item)
+    {
+        if (_index >= _data.Length - 1) return false;
+        _index++;
+        _data[_index] = key;
+        return true;
+    }
 
     //resize function
 
     //remove function
-
+    public void Remove(T item)
+    {
+        for (int i = 0; i < _size; i++)
+        {
+            if (_array[i] == item)
+            {
+                // Shift elements to the left
+                for (int j = i; j < _size - 1; j++)
+                {
+                    _array[j] = _array[j + 1];
+                }
+                _size--;
+                return;
+            }
+        }
+    }
     //findby index function
 
     // filter function
