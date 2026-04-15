3a5cf35cba3690d7076227fa102729e612f3e6d8
Vanderflys <85625559+Vanderflys@users.noreply.github.com>
2026-04-15 16:21:22 +0200

    Added add(), iterator, and enumerator. Increased max tasks count to 20

--- DataStructures/MyHashMap.cs
+++ DataStructures/MyHashMap.cs
@@ -1,13 +1,28 @@
 
-public class MyHashMap<T> : IMyCollection<T>
+public class MyHashMap<T> : IMyCollection<T> where T : notnull
 {
-    public int Count => throw new NotImplementedException();
+    public Dictionary<T, T> Value { get; private set; }
+
+    public MyHashMap(T data)
+    {
+        Value = data.GetType()
+            .GetProperties()
+            .Where(p => p.PropertyType == typeof(T))
+            .ToDictionary(p => (T)p.GetValue(data)!, p => (T)p.GetValue(data)!);
+    }
+
+    public MyHashMap()
+    {
+        Value = new Dictionary<T, T>();
+    }
+
+    public int Count => Value.Count;
 
     public bool Dirty { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
     public void Add(T item)
     {
-        throw new NotImplementedException();
+        Value.Add(item, item);
     }
 
     public IMyCollection<T> Filter(Func<T, bool> predicate)
@@ -22,12 +37,15 @@ public class MyHashMap<T> : IMyCollection<T>
 
     public IEnumerator<T> GetEnumerator()
     {
-        throw new NotImplementedException();
+        foreach (var value in Value.Values)
+        {
+            yield return value;
+        }
     }
 
     public IMyIterator<T> GetMyIterator()
     {
-        throw new NotImplementedException();
+        return new MyIterator(Value.Values.ToArray());
     }
 
     public R Reduce<R>(Func<R, T, R> accumulator)
@@ -49,4 +67,35 @@ public class MyHashMap<T> : IMyCollection<T>
     {
         throw new NotImplementedException();
     }
+
+    private class MyIterator : IMyIterator<T>
+    {
+        private readonly T[] _values;
+        private int _currentIndex;
+
+        public MyIterator(T[]? array)
+        {
+            _values = array ?? Array.Empty<T>();
+            _currentIndex = -1;
+        }
+
+        public bool HasNext()
+        {
+            return _currentIndex + 1 < _values.Length;
+        }
+
+        public T Next()
+        {
+            if (!HasNext()) throw new InvalidOperationException("No more elements.");
+
+            _currentIndex++;
+            return _values[_currentIndex];
+        }
+
+        public void Reset()
+        {
+            _currentIndex = -1;
+        }
+    }
 }
+
